4fdf0cf8c57073027db443517f8105fc3b306f8b
Allysson05 <sjomy20@gmail.com>
2026-07-15 23:40:11 -0500

    Ejercicio 1 y 4

--- Tarea_Estructura_Datos/src/Ejercicio_01.java
+++ Tarea_Estructura_Datos/src/Ejercicio_01.java
@@ -1,12 +1,131 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author allys
- */
-public class Ejercicio_01 {
-    
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+   }
+   class ListaEnlazadaSimple{
+    Node head;
+    public ListaEnlazadaSimple insert(int data) {
+        Node new_Node = new Node(data);
+
+        if (head == null) {
+            head = new_Node;
+        } else {
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new_Node;
+        }
+
+        return this;
+    }
+
+    public void print() {
+        Node current = head;
+        System.out.println("Lista enlazada:");
+
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public ListaEnlazadaSimple insertStart(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+
+        } else {
+            newNode.next = head;
+            head = newNode;
+
+        }
+        return this;
+    }
+
+    public void search(int key) {
+        Node current = head;
+
+        while (current != null) {
+            if (current.data == key) {
+                System.out.println("Elemento " + key + " encontrado");
+                return;
+            }
+            current = current.next;
+        }
+
+        System.out.println("Elemento " + key + " no encontrado");
+    }
+
+    public void delete(int key) {
+        if (head == null) {
+            System.out.println(key + " not found");
+            return;
+        }
+        Node current = head;
+        Node prev = null;
+        while (current != null && current.data != key) {
+            prev = current;
+            current = current.next;
+        }
+        if (current == null) {
+            System.out.println(key + " not found");
+            return;
+        }
+        if (prev == null) {
+            head = current.next;
+        } else {
+            prev.next = current.next;
+        }
+        current.next = null;
+        System.out.println(key + " fue eliminado");
+    }
+    public void deleteMax(){
+        if(head == null){
+            return;
+        }
+        int mayor = head.data;
+        Node current = head;
+        
+        while(current != null){
+            if(current.data > mayor){
+                mayor = current.data;
+            }
+            current = current.next;
+        }
+        delete(mayor);
+    }
+   }
+
+  public class Ejercicio_01 {
+    public static void main(String[] args) {
+        ListaEnlazadaSimple lista = new ListaEnlazadaSimple();
+        System.out.println("-----LISTA INSERTADA DE NUMEROS------");
+        lista.insert(10);
+        lista.insert(20);
+        lista.insert(30);
+        lista.print();
+        System.out.println("-------AÑADIR NUMEROS AL INICIO-------");
+        lista.insertStart(40);
+        lista.insertStart(50);
+        lista.print();
+        System.out.println("-------------BUSCAR UN NUMERO--------");
+        lista.search(20);
+        lista.search(90);
+        lista.print();
+        System.out.println("--------NUMEROS ELIMINADOS-------");
+        lista.delete(20);
+        lista.delete(30);
+        lista.print();
+        System.out.println("--------ELIMINAR EL VALOR MAYOR-------");
+        lista.deleteMax();
+        lista.print();
+    }
 }
+
--- Tarea_Estructura_Datos/src/Ejercicio_04.java
+++ Tarea_Estructura_Datos/src/Ejercicio_04.java
@@ -1,12 +1,102 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author allys
- */
-public class Ejercicio_04 {
-    
+class Nodo {
+    String nombre;
+    Nodo izquierdo;
+    Nodo derecho;
+
+    public Nodo(String nombre) {
+        this.nombre = nombre;
+        izquierdo = null;
+        derecho = null;
+    }
 }
+class ArbolEnfermedades {
+
+    Nodo raiz;
+
+    public void crearArbol() {
+
+        raiz = new Nodo("Enfermedades cardiovasculares");
+
+        raiz.izquierdo = new Nodo("Cardiopatias");
+        raiz.derecho = new Nodo("Enfermedades vasculares");
+
+        raiz.izquierdo.izquierdo = new Nodo("Infarto");
+        raiz.izquierdo.derecho = new Nodo("Insuficiencia");
+
+        raiz.derecho.izquierdo = new Nodo("Aneurisma");
+        raiz.derecho.derecho = new Nodo("Aterosclerosis");
+
+        raiz.izquierdo.izquierdo.izquierdo = new Nodo("Arritmias");
+
+        raiz.izquierdo.derecho.izquierdo = new Nodo("Shock cardiogenico");
+    }
+public void mostrarNodos(Nodo nodo) {
+
+    if(nodo != null) {
+
+        System.out.println(nodo.nombre);
+
+        mostrarNodos(nodo.izquierdo);
+        mostrarNodos(nodo.derecho);
+    }
+}
+public void mostrarPadresHijos(Nodo nodo) {
+
+    if (nodo != null) {
+
+        if (nodo.izquierdo != null || nodo.derecho != null) {
+
+            System.out.print("Padre: " + nodo.nombre);
+
+            if (nodo.izquierdo != null) {
+                System.out.print(" | Hijo Izq: "
+                        + nodo.izquierdo.nombre);
+            }
+
+            if (nodo.derecho != null) {
+                System.out.print(" | Hijo Der: "
+                        + nodo.derecho.nombre);
+            }
+
+            System.out.println();
+        }
+
+        mostrarPadresHijos(nodo.izquierdo);
+        mostrarPadresHijos(nodo.derecho);
+    }
+}
+public void mostrarHojas(Nodo nodo) {
+
+    if (nodo != null) {
+
+        if (nodo.izquierdo == null
+                && nodo.derecho == null) {
+
+            System.out.println(nodo.nombre);
+        }
+
+        mostrarHojas(nodo.izquierdo);
+        mostrarHojas(nodo.derecho);
+    }
+}
+}
+public class Ejercicio_04 {
+    public static void main(String[] args) {
+
+    ArbolEnfermedades arbol = new ArbolEnfermedades();
+
+    arbol.crearArbol();
+
+    System.out.println("TODOS LOS NODOS");
+    arbol.mostrarNodos(arbol.raiz);
+
+    System.out.println("\nPADRES E HIJOS");
+    arbol.mostrarPadresHijos(arbol.raiz);
+
+    System.out.println("\nNODOS HOJA");
+    arbol.mostrarHojas(arbol.raiz);
+    
+    } 
+}
+ 
